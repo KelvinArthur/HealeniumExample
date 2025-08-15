724c3205206e1f59d78ba711a90f97e8fbd5c106
Kelvin <kelvinmathewarthur009@gmail.com>
2025-08-16 00:09:39 +0530

    Initial commit

---
+++ src/test/java/GooglePageTest.java
@@ -0,0 +1,20 @@
+import com.epam.healenium.SelfHealingDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.net.MalformedURLException;
+
+public class GooglePageTest {
+
+    public static void main (String []args) throws MalformedURLException {
+
+        WebDriver delegate = new ChromeDriver();
+        SelfHealingDriver driver = SelfHealingDriver.create((WebDriver) delegate);
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//a[text()='About']")).click();
+
+
+    }
+}
